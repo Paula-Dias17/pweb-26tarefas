712d0493c8d3d445ca13a3c4d1ed9807e296fded
Paula-Dias17 <m.paula3405@gmail.com>
2026-05-23 11:21:51 -0300

    conclui a tarefa03

--- tarefa03/app/views.py
+++ tarefa03/app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def usuarios(request):
+    lista_user = [
+        {"nome": "Paula", "matricula": 20, "idade": 17, "cidade": "BS"},
+        {"nome": "Anna Vitória", "matricula": 8, "idade": 17, "cidade": "BS"},
+        {"nome": "Camily", "matricula": 34, "idade": 64, "cidade": "BS"},
+        {"nome": "Lula", "matricula": 13, "idade": 18, "cidade": "Presidencia"},
+        {"nome": "Bolsonaro", "matricula": 22, "idade": 19, "cidade": "Prisão"},
+    ]
+
+    context = {
+        "usuarios": lista_user,
+    }
+  
+    return render(request, "usuarios.html", context)
