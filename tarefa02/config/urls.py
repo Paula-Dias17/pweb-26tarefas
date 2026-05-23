2d6ca1f301f4548bc0f1ac4ccb460b8e95cbd4a5
Paula-Dias17 <m.paula3405@gmail.com>
2026-05-23 10:27:32 -0300

    corriji o erro de apps

--- tarefa02/config/urls.py
+++ tarefa02/config/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('tarefas.url')),
+    path('', include('exemplo.urls')),
     path('admin/', admin.site.urls),
 ]
--- tarefa02/exemplo/apps.py
+++ tarefa02/exemplo/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class ExemploConfig(AppConfig):
+class Meuappconfig(AppConfig):
     name = 'exemplo'
