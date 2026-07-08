1075dcc3578668d03a8014de69c0c512a7aef774
Paula-Dias17 <m.paula3405@gmail.com>
2026-07-08 17:12:42 -0300

    fix nomes, e migrations

--- tarefa05/blog/models.py
+++ tarefa05/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=40)
+    texto = models.TextField()
+    imagem = models.ImageField(upload_to='imgs/')
+    data_publicacao = models.DateField()
+
+    def __str__(self):
+        return self.titulo
+    # def __str__(self):
+    #     return (f"Titulo: {self.titulo} | texto: {self.texto}")
--- tarefa05/blog/views.py
+++ tarefa05/blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts})
+
+
+# def post(request, id):
+#     postagem = get_object_or_404(Post, id=id)
+#     return render(request, 'post.html', {'post': postagem})
